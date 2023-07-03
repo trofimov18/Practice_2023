2952ea1cd79fc99ce89c1e7b51b1f4e54cb2b976
RARadchenko <raradchenko@users.noreply.github.com>
2023-07-03 19:18:58 +0300

    Added logic of reading and creating .cfg file

--- main.c
+++ main.c
@@ -20,15 +20,16 @@
 
 
 int main(int argc, char* argv[]) {
-
 	// шлях до бази данних
 	char DB_path[200];
-
 	// зчитаний ключ з конфігураційного файлу
-	int license_key;
-
+	int license_key = 0;
 	// генерація імені файла
 	char cfg_filename[200];
+	// валідність ключа
+	int program_activate = 1;
+
+
 	// отримуємо назву
 	strcpy(cfg_filename, argv[0]);
 	// отримуємо довжину рядка
@@ -40,26 +41,19 @@ int main(int argc, char* argv[]) {
 	cfg_filename[lengh - 2] = 'f';
 	cfg_filename[lengh - 3] = 'c';
 	
-	// виводимо шлях поточної програми
-	printf("name of file: %s", cfg_filename);
 
-	// редагуємо ключ та назву БД
-	printf("\nkey: %d\n", key_generate());
-	edit_key(cfg_filename, 2921221);
-	edit_pathDB(cfg_filename, "LongExampleDataBaseName.dat");
-	make_cfg_file(cfg_filename);
-	
 	// перевірка чи вдалося зчитати
 	if ((read_pathDB(cfg_filename, DB_path) != 0) || (read_key(cfg_filename, &license_key))) {
 		// вивід повідомлення про помилку
-		printf("\nError occurupted\n");
-		return 1;
+		printf("\nRead .cfg file error\nCreating new .cfg file");
+		// створення базового конфігураційного файлу
+		make_cfg_file(cfg_filename);
+		printf("\n.cfg file created");
 	}
 	// виведення назви бази данних
-	else {
-		printf("\nname of DB file: %s\nkey: %d", DB_path, license_key);
-		printf("\nis valid %d", key_valid(license_key));
-	}
+	printf("\nYou now working with DB file: %s\nkey: %d", DB_path, license_key);
+	program_activate = key_generate(license_key);
+	program_activate == 0 ? printf("\nYou using full version of programm") : printf("\nLicense key invalid you can`t use advansed programm funcs");
 
 	return 0;
 }
