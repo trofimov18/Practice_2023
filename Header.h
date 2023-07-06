0fee8063c59d440a591fdfe2f3073a36eb839f8b
vovksofiia <137784025+vovksofiia@users.noreply.github.com>
2023-07-06 15:20:45 +0200

    Header.h was added

---
+++ Header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#define MAX 100
+#define KEY_LEN 8
+
+int new_rec();
+int delete_rec();
+int find_rec(int number);
+int all_recs();
+int update();
+int after_date();
+int salary();
+int key();
+
+#endif
